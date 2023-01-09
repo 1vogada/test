bc528745f651db59089a359abbfb3a42a5ef570c
Damyan03 <112628176+Damyan03@users.noreply.github.com>
2023-01-09 11:36:44 +0100

    Dialogue and bridge

--- src/GameObjects/Bridge.ts
+++ src/GameObjects/Bridge.ts
@@ -1,12 +1,12 @@
 import CanvasUtil from '../CanvasUtil.js';
 import GameObject from './GameObject.js';
 
-export default class Helper extends GameObject {
+export default class Rock extends GameObject {
   public constructor(posX: number, posY: number) {
     super();
+    this.image = CanvasUtil.loadNewImage('./assets/bridge.png');
     this.posX = posX;
     this.posY = posY;
-    this.image = CanvasUtil.loadNewImage('./assets/helper.png');
   }
 
   /**
@@ -14,5 +14,13 @@ export default class Helper extends GameObject {
    */
   public override update(): void {
     console.log(this.posX);
+  }
+
+  public setPosX(posX: number): void {
+    this.posX = posX;
+  }
+
+  public setPosY(posY: number): void {
+    this.posY = posY;
   }
 }
--- src/Scenes/LevelOne.ts
+++ src/Scenes/LevelOne.ts
@@ -7,6 +7,7 @@ import CanvasUtil from '../CanvasUtil.js';
 import GameObject from '../GameObjects/GameObject.js';
 import Rock from '../GameObjects/Rock.js';
 import Helper from '../GameObjects/Helper.js';
+import Bridge from '../GameObjects/Bridge.js';
 
 export default class LevelOne extends Scene {
   private player: Player;
@@ -15,62 +16,125 @@ export default class LevelOne extends Scene {
 
   private gameObjects: GameObject[] = [];
 
-  private playableAreaX:number;
+  private playableAreaLeftX:number;
 
-  private playableAreaY:number;
+  private playableAreaLeftY:number;
 
-  private playableAreaMaxX:number;
+  private playableAreaLeftMaxX:number;
 
-  private playableAreaMaxY: number;
+  private playableAreaLeftMaxY: number;
+
+  private playableAreaBridgeX:number;
+
+  private playableAreaBridgeY:number;
+
+  private playableAreaBridgeMaxX:number;
+
+  private playableAreaBridgeMaxY: number;
 
   private isUsing: boolean;
 
   private hasRock: boolean;
 
+  private isTalking: boolean;
+
+  private isCorrect: boolean;
+
   public constructor(maxX: number, maxY: number) {
     super(maxX, maxY);
     this.background = CanvasUtil.loadNewImage('./assets/backgroundLevelOne.png');
     this.player = new Player();
-    this.helper = new Helper(600, 700);
+    this.gameObjects.push(new Helper(600, 700));
 
-    this.playableAreaMaxX = maxX / 2;
-    this.playableAreaMaxY = 865;
-    this.playableAreaX = 0;
-    this.playableAreaY = 290;
+    this.playableAreaLeftMaxX = maxX / 2;
+    this.playableAreaLeftMaxY = 865;
+    this.playableAreaLeftX = 0;
+    this.playableAreaLeftY = 290;
+
+    this.playableAreaBridgeMaxX = 1350;
+    this.playableAreaBridgeMaxY = 850;
+    this.playableAreaBridgeX = maxX / 2;
+    this.playableAreaBridgeY = 750;
 
     this.gameObjects.push(new Rock(600, 300));
     this.gameObjects.push(new Rock(300, 550));
     this.gameObjects.push(new Rock(550, 750));
+    this.gameObjects.push(new Bridge(1050, 300));
+    this.gameObjects.push(new Bridge(1150, 550));
+    this.gameObjects.push(new Bridge(1250, 750));
 
     this.isUsing = false;
     this.hasRock = false;
+    this.isTalking = false;
+    this.isCorrect = false;
   }
 
   public processInput(keyListener: KeyListener): void {
+    const playerPosY: number = this.player.getPosY() + this.player.getHeight();
+    const playerPosX: number = this.player.getPosX();
+
     if (keyListener.keyPressed(KeyListener.KEY_ESC)) window.location.reload();
-    if (keyListener.isKeyDown(KeyListener.KEY_W) && (this.player.getPosY() + this.player.getHeight() > this.playableAreaY)) this.player.moveUp();
-    if (keyListener.isKeyDown(KeyListener.KEY_S) && (this.player.getPosY() + this.player.getHeight() < this.playableAreaMaxY)) this.player.moveDown();
-    if (keyListener.isKeyDown(KeyListener.KEY_A) && (this.player.getPosX() > this.playableAreaX)) this.player.moveLeft();
-    if (keyListener.isKeyDown(KeyListener.KEY_D) && (this.player.getPosX() < this.playableAreaMaxX)) this.player.moveRight();
+    if (keyListener.isKeyDown(KeyListener.KEY_W)) {
+      if (!this.isCorrect && playerPosY > this.playableAreaLeftY) this.player.moveUp();
+      if (this.isCorrect) {
+        if (playerPosX > this.playableAreaBridgeX && playerPosY > this.playableAreaBridgeY) this.player.moveUp();
+        else if (playerPosX < this.playableAreaBridgeX && playerPosY > this.playableAreaLeftY) this.player.moveUp();
+      }
+    }
+    if (keyListener.isKeyDown(KeyListener.KEY_S)) {
+      if (!this.isCorrect && playerPosY < this.playableAreaLeftMaxY) this.player.moveDown();
+      if (this.isCorrect) {
+        if (playerPosY < this.playableAreaLeftMaxY) this.player.moveDown();
+      }
+    }
+    if (keyListener.isKeyDown(KeyListener.KEY_A)) {
+      if (!this.isCorrect && playerPosX > this.playableAreaLeftX) this.player.moveLeft();
+      if (this.isCorrect) {
+        if (playerPosX > this.playableAreaLeftX) this.player.moveLeft();
+      }
+    }
+    if (keyListener.isKeyDown(KeyListener.KEY_D)) {
+      if (!this.isCorrect && playerPosX < this.playableAreaLeftMaxX) this.player.moveRight();
+      if (this.isCorrect) {
+        if (playerPosY + 10 > this.playableAreaBridgeY && playerPosX < this.playableAreaBridgeMaxX) this.player.moveRight();
+        if (playerPosY < this.playableAreaBridgeY && playerPosX + 10 < this.playableAreaLeftMaxX) this.player.moveRight();
+      }
+    }
     if (keyListener.keyPressed(KeyListener.KEY_E)) this.isUsing = true;
+    if (keyListener.keyPressed(KeyListener.KEY_2) && this.isTalking) this.isCorrect = true;
   }
 
   public update(elapsed: number): Scene {
+    // Rock pickup and drop
     this.gameObjects.forEach((rock: Rock) => {
-      if (this.isUsing && this.player.collideWithObject(rock) && !this.hasRock) {
-        this.hasRock = true;
-        rock.setStatusCarried(true);
-      } else if (this.isUsing && this.player.collideWithObject(rock) && this.hasRock) {
-        this.hasRock = false;
-        rock.setStatusCarried(false);
+      if (this.isUsing && this.player.collideWithObject(rock) && rock instanceof Rock) {
+        if (!this.hasRock) {
+          this.hasRock = true;
+          rock.setStatusCarried(true);
+        } else if (this.hasRock) {
+          this.hasRock = false;
+          rock.setStatusCarried(false);
+        }
       }
     });
+    // rock movement when picked up
     this.gameObjects.forEach((object: GameObject) => {
       if (object instanceof Rock && this.player.collideWithObject(object) && object.getStatusCarried() && this.hasRock) {
         object.setPosX(this.player.getPosX() + this.player.getWidth() * 0.65);
         object.setPosY(this.player.getPosY() + this.player.getHeight() * 0.45);
       }
+      if (object instanceof Helper && this.player.collideWithObject(object) && this.isUsing) {
+        this.isTalking = true;
+      }
     });
+    if (this.isCorrect) {
+      this.gameObjects.forEach((object: GameObject) => {
+        if (object instanceof Bridge && object.getPosY() < 750) {
+          object.setPosY(object.getPosY() + elapsed * 0.2);
+        }
+      });
+    }
+
     this.isUsing = false;
     return null;
   }
@@ -80,11 +144,25 @@ export default class LevelOne extends Scene {
     CanvasUtil.drawImage(canvas, this.background, 0, 0);
     this.gameObjects.forEach((object: GameObject) => object.render(canvas));
     this.player.render(canvas);
+    // Render forward things closer and further things behind
     this.gameObjects.forEach((object: GameObject) => {
-      if (this.player.collideWithObject(object) && this.player.collideWithObject(object) && this.player.getPosY() + this.player.getHeight() < object.getPosY() + object.getHeight()) {
+      if (object instanceof Rock && this.player.collideWithObject(object) && this.player.collideWithObject(object) && this.player.getPosY() + this.player.getHeight() < object.getPosY() + object.getHeight()) {
         object.render(canvas);
       }
     });
-    this.helper.render(canvas);
+
+    this.gameObjects.forEach((object: GameObject) => {
+      if (this.isTalking) {
+        CanvasUtil.fillRectangle(canvas, 50, 50, 500, 200, 'black');
+        CanvasUtil.writeTextToCanvas(canvas, 'What u do', 60, 100, 'left', 'sans-serif', 25, 'white');
+        CanvasUtil.writeTextToCanvas(canvas, '1', 60, 130, 'left', 'sans-serif', 25, 'white');
+        CanvasUtil.writeTextToCanvas(canvas, '2', 60, 160, 'left', 'sans-serif', 25, 'white');
+        CanvasUtil.writeTextToCanvas(canvas, '3', 60, 190, 'left', 'sans-serif', 25, 'white');
+        if (this.isCorrect) {
+          CanvasUtil.fillRectangle(canvas, 50, 50, 500, 200, 'black');
+          CanvasUtil.writeTextToCanvas(canvas, 'U can pass now', 60, 100, 'left', 'sans-serif', 25, 'white');
+        }
+      }
+    });
   }
 }
