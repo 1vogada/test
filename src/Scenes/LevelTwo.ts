d9182b8c5c8c5782f336f16e21e711ff433b4013
TonevS <tone0001@hz.nl>
2023-01-17 11:41:16 +0100

    Fix later

--- src/Scenes/LevelTwo.ts
+++ src/Scenes/LevelTwo.ts
@@ -1,13 +1,13 @@
-/* eslint-disable max-len */
-/* eslint-disable jsdoc/require-jsdoc */
-import KeyListener from '../KeyListener.js';
-import Scene from './Scene.js';
-import Player from '../Player.js';
-import CanvasUtil from '../CanvasUtil.js';
-import GameObject from '../GameObjects/GameObject.js';
+// /* eslint-disable max-len */
+// /* eslint-disable jsdoc/require-jsdoc */
+// import KeyListener from '../KeyListener.js';
+// import Scene from './Scene.js';
+// import Player from '../Player.js';
+// import CanvasUtil from '../CanvasUtil.js';
+// import GameObject from '../GameObjects/GameObject.js';
 
-export default class LevelTwo extends Scene {
-  private player: Player;
+// export default class LevelTwo extends Scene {
+//   private player: Player;
 
-  private gameObjects: GameObject[] = [];
-}
+//   private gameObjects: GameObject[] = [];
+// } - Fix later
